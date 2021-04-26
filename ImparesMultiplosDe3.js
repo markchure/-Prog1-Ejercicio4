d88891011a6f17975270c0f5f81765b542fa8bbd
Marcos Churi <51281680+markchure@users.noreply.github.com>
2021-04-26 01:47:08 -0300

    Update ImparesMultiplosDe3.js

--- ImparesMultiplosDe3.js
+++ ImparesMultiplosDe3.js
@@ -1,3 +1,4 @@
+//Pedir el ingreso de un número n. Mostrar la suma de todos los impares que sean múltiplos de 3 entre 1 y n. Ej. si se ingresa 16, se muestra 27.
 var n = parseInt(prompt("Ingrese un número: "));
 var suma = 0;
 
